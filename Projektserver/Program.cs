add9a970b8234831b5a124b32b006e83393426b8
EliaSpendelhofer <Elias.spendelhofer@josephinum.at>
2024-05-07 14:01:32 +0200

    client bekommt X oder O geschickt kann es aber noch nicht verarbeiten

--- Projektserver/Program.cs
+++ Projektserver/Program.cs
@@ -1,27 +1,27 @@
-﻿using System.Net.Sockets;
+﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Xml;
+using System.Threading;
 
 namespace server
 {
     class Program
     {
+        private static Semaphore semaphore = new Semaphore(1, 1); // Semaphore mit Anfangswert 1
+        private static string symbol = ""; // Variable für das Symbol
 
-        // Main Method
+        // Main Method 
         static void Main(string[] args)
         {
             Console.Title = "Socket Server";
 
             IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
 
-
-
             foreach (IPAddress i in hostEntry.AddressList)
             {
                 Console.WriteLine(i);
             }
-
 
             Console.WriteLine("Listening for messages...");
 
@@ -30,25 +30,39 @@ namespace server
                 SocketType.Stream,
                 ProtocolType.Tcp);
 
-
             IPAddress serverIP = IPAddress.Any;
             IPEndPoint serverEP = new IPEndPoint(serverIP, 11111);
 
             serverSock.Bind(serverEP);
             /*try
-          
-                serverSock.Bind(serverEP);
-            }
-            catch (System.Net.Sockets.SocketException sockEx)
-            {
-                int errcode = sockEx.ErrorCode;
-                Console.WriteLine(errcode);
-            }*/
+
+               serverSock.Bind(serverEP);
+           }
+           catch (System.Net.Sockets.SocketException sockEx)
+           {
+               int errcode = sockEx.ErrorCode;
+               Console.WriteLine(errcode);
+           }*/
             serverSock.Listen(10);
 
             while (true)
             {
                 Socket connection = serverSock.Accept();
+
+                // Semaphore übernehmen
+                semaphore.WaitOne();
+                if (symbol == "")
+                {
+                    symbol = "X"; // Erster Client bekommt das Symbol "X"
+                    Byte[] symbolBytes = Encoding.UTF8.GetBytes(symbol);
+                    connection.Send(symbolBytes); // Sende das Symbol "X" an den Client
+                }
+                else if (symbol == "X")
+                {
+                    symbol = "O"; // Zweiter Client bekommt das Symbol "O"
+                    Byte[] symbolBytes = Encoding.UTF8.GetBytes(symbol);
+                    connection.Send(symbolBytes);// Sende das Symbol "O" an den Client
+                } 
 
                 Byte[] serverBuffer = new Byte[1024];
                 String message = String.Empty;
@@ -58,6 +72,11 @@ namespace server
                 message += Encoding.UTF8.GetString(serverBuffer, 0, bytes);
 
                 Console.WriteLine(message);
+               
+                
+
+                // Semaphore freigeben
+                semaphore.Release();
 
                 connection.Close();
             }
